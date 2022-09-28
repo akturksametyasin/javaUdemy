07d387ae0248b555d80812ca4ad9c5b2fe93076b
Samet Yasin Aktürk <78413314+akturksametyasin@users.noreply.github.com>
2022-09-28 15:04:37 +0300

    stringler

---
+++ Stringler/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String a = "Java\tProgramlama\tDili";       // \t 1 tab boşluğu bırakır.
+        
+        System.out.println(a);
+        
+        String b = "Java\nProgramlama\nDili";       // \n bir alt satıra indirir.
+        
+        System.out.println(b);
+        
+    }
+    
+}
