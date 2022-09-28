21919e7ce34d9e4d62f7e15ab0087849b1f5a186
Samet Yasin Aktürk <78413314+akturksametyasin@users.noreply.github.com>
2022-09-28 15:01:28 +0300

    degiskenler

---
+++ Degiskenler/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+    public static void main (String[] args){
+    
+        int a = 10;
+        int b = a * 2 ;
+        int c = b - 3;
+        int toplam = 2 * a + b - c;
+        
+        System.out.println("Toplam değer = " + toplam);
+               
+    }
+}
