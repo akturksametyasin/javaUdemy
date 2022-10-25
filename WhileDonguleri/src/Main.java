0b9745f08a79a38bdd07f728c3e061ce5169e1cc
Samet Yasin Aktürk <78413314+akturksametyasin@users.noreply.github.com>
2022-10-25 11:18:27 +0300

    whileDonguleri

---
+++ WhileDonguleri/src/Main.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author samet
+ */
+public class Main {
+   
+    public static void main(String[] args) {
+        
+        /*
+        while(döngü koşulu){
+            // koşul doğru olduğu sürece çalışır.
+        }
+        */
+        
+        /*
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+        */
+        
+        /*
+        int i = 10;
+        while(i < 100){
+            System.out.println("i = " + i);
+            i+=3;
+        }
+        */
+        
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz : ");
+        int sayi = scanner.nextInt();
+        int faksayi = sayi;
+        int faktoriyel = 1;
+        
+        while(sayi > 0){
+            faktoriyel *= sayi;
+            sayi--;
+        }
+        System.out.println(faksayi + "! = " + faktoriyel);
+        */
+        
+        int i = 0;
+        while(i < 10){      //sonsuz dongu
+            System.out.println("i = " + i);
+            i--;
+        }
+        
+    }
+}
